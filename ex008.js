2a0e1811b0aa487116d04d0df8686814c6e52569
Ruãn Santos <pedrirodrigues234@gmail.com>
2023-07-28 13:46:47 -0400

    sei la

---
+++ ex008.js
@@ -0,0 +1,6 @@
+var vel = 80.8
+console.log(`A velocidade de seu carro é de ${vel}Km/h`)
+if (vel > 70) { //Condição Simples!
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Nunca esqueça o sinto de segurança, obrigado pela atenção`)
---
+++ node_15d9ef6848e61.tmp.js
@@ -0,0 +1,5 @@
+var vel = 80.8
+console.log(`A velocidade de seu carro é de ${vel}Km/h`)
+if (vel > 70) { (`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Nunca esqueça o sinto de segurança, obrigado pela atenção`)
